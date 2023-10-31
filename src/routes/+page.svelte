fad997fc95e66fb791f71669681b1fe2280293e3
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-10-31 11:22:17 +0000

    basic load and calculation, added linter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,6 @@
 <script>
-    import {onMount} from 'svelte'
+    import Savings from '../lib/Savings.svelte';
     export let data
-    let dataset = data.filteredData
-    onMount(async() => {
-        console.log(data)
-    })
 </script>
 
-<ul>
-{#each dataset as entry}
-    <li>{new Date(entry.start_date).toDateString()}</li>
-{/each}
-</ul>
+<Savings data={data.filteredData}/>
