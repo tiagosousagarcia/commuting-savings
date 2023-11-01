125a148eacb758792679f723e243faea1ed1d784
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-11-01 18:11:41 +0000

    trying to get authorisation code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { onMount } from 'svelte'
     onMount(() => {
-        window.location = "http://www.strava.com/oauth/authorize?client_id=[CLIENT_ID]&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read"
+        window.location = "http://www.strava.com/oauth/authorize?client_id=FAKE&response_type=code&redirect_uri=http://localhost:5173/exchange_token&approval_prompt=force&scope=read"
     })
 </script>
